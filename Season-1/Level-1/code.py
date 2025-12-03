1837c9899c9e18c126c8f5f54c9d4ea429512d89
Shiguang Hao <112836181+weitero@users.noreply.github.com>
2025-12-03 21:22:27 +0000

    Fix payment calculation by ensuring amounts are handled as Decimals

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -12,6 +12,7 @@ Follow the instructions below to get started:
 '''
 
 from collections import namedtuple
+from decimal import Decimal
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
@@ -21,9 +22,9 @@ def validorder(order: Order):
 
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            net += Decimal(str(item.amount))
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            net -= Decimal(str(item.amount * item.quantity))
         else:
             return "Invalid item type: %s" % item.type
 
